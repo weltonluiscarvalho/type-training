b06d3e65a1f498a029115d837f5d553ec3e94439
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-04-20 11:31:46 -0300

    refactor the main widget into a class

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
 
 class TypeCheckLabel(QLabel):
     def __init__(self, target_text, parent=None):
         super().__init__(parent)
         self.target_text = target_text
         self.current_position = 0
-
+        self.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        font = QFont()
+        font.setPointSize(50)
+        self.setFont(font)
+        self.setWordWrap(True)
+        self.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
         self.update_dysplay()
 
     def update_dysplay(self):
         html_text = self.target_text
         self.setText(html_text)
 
-app = QApplication()
 
-texto = "O que fazemos hoje ecoa pela eternidade"
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Type Training")
 
-label = TypeCheckLabel(target_text=texto)
-label.setAlignment(Qt.AlignCenter)
+        text = "O que fazemos hoje ecoa pela eternidade"
 
-font = QFont()
-font.setPointSize(50)
-label.setFont(font)
-label.setWordWrap(True)
-label.setTextInteractionFlags(Qt.TextSelectableByMouse)
+        self.type_label = TypeCheckLabel(text)
+        layout = QVBoxLayout()
+        layout.addWidget(self.type_label)
+        self.setLayout(layout)
 
-label.show()
 
-app.exec()
+
+if __name__ == "__main__":
+    app = QApplication()
+    window = MainWindow()
+    window.show()
+    app.exec()

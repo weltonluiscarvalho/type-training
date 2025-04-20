65c9ae28c8e62eda344ea2a545765bf5548b1e83
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-04-20 11:59:55 -0300

    create logic from update_dysplay method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QFont
+from PySide6.QtGui import QColor
 from PySide6.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
 
 class TypeCheckLabel(QLabel):
@@ -7,16 +7,28 @@ class TypeCheckLabel(QLabel):
         super().__init__(parent)
         self.target_text = target_text
         self.current_position = 0
+        self.correct_color = QColor("#2ecc71")
+        self.wrong_color = QColor("#e74c3c")
+        self.default_color = QColor("#000000")
+
         self.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        font = QFont()
-        font.setPointSize(50)
-        self.setFont(font)
+        self.setStyleSheet("font-size: 50px; padding: 20px; border: 1px solid black;") 
         self.setWordWrap(True)
         self.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
         self.update_dysplay()
+        self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
 
     def update_dysplay(self):
-        html_text = self.target_text
+        html_text = ""
+
+        for i, char in enumerate(self.target_text):
+            if i < self.current_position:
+                html_text += f'<span style="color:{self.correct_color.name()}">{char}</span>'
+            elif i == self.current_position:
+                html_text += f'<span style="color:{self.default_color.name()}; background-color:#f1c40f;">{char}</span>'
+            else:
+                html_text += f'<span style="color:{self.default_color.name()}">{char}</span>'
+
         self.setText(html_text)
 
 

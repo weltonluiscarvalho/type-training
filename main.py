7a1c24c7054b41b831cd1219c9012a1698224e83
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-04-19 22:01:34 -0300

    create a method to update the text of the label

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication, QLabel
 
 class TypeCheckLabel(QLabel):
-    def __init__(self, parent=None):
+    def __init__(self, target_text, parent=None):
         super().__init__(parent)
+        self.target_text = target_text
+        self.current_position = 0
+
+        self.update_dysplay()
+
+    def update_dysplay(self):
+        html_text = self.target_text
+        self.setText(html_text)
 
 app = QApplication()
 
 texto = "O que fazemos hoje ecoa pela eternidade"
 
-label = TypeCheckLabel(texto)
+label = TypeCheckLabel(target_text=texto)
 label.setAlignment(Qt.AlignCenter)
 
 font = QFont()

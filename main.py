b62d5da51247b2bde8a8c14a76febdd4af31430d
Welton Carvalho <welton.carvalho.dev@gmail.com>
2025-04-20 14:13:55 -0300

    set background in red when type the wrong character

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PySide6.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
 class TypeCheckLabel(QLabel):
     def __init__(self, target_text, parent=None):
         super().__init__(parent)
+        self.errors = 0
         self.target_text = target_text
         self.current_position = 0
         self.correct_color = QColor("#2ecc71")
@@ -15,17 +16,17 @@ class TypeCheckLabel(QLabel):
         self.setStyleSheet("font-size: 50px; padding: 20px; border: 1px solid black;") 
         self.setWordWrap(True)
         self.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
-        self.update_dysplay()
+        self.update_dysplay("#f1c40f")
         self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
 
-    def update_dysplay(self):
+    def update_dysplay(self, background_color):
         html_text = ""
 
         for i, char in enumerate(self.target_text):
             if i < self.current_position:
                 html_text += f'<span style="color:{self.correct_color.name()}">{char}</span>'
             elif i == self.current_position:
-                html_text += f'<span style="color:{self.default_color.name()}; background-color:#f1c40f;">{char}</span>'
+                html_text += f'<span style="color:{self.default_color.name()}; background-color:{background_color};">{char}</span>'
             else:
                 html_text += f'<span style="color:{self.default_color.name()}">{char}</span>'
 
@@ -41,17 +42,22 @@ class TypeCheckLabel(QLabel):
         if event.key() == Qt.Key.Key_Backspace:
             if self.current_position > 0:
                 self.current_position -= 1
-                self.update_dysplay()
+                self.update_dysplay("#f1c40f")
             return
 
         expected_char = self.target_text[self.current_position]
         if event.text() == expected_char:
             self.current_position += 1
-            self.update_dysplay()
+            self.update_dysplay("#f1c40f")
 
             if self.current_position == len(self.target_text):
                 self.setText(f'<span style="color:{self.correct_color.name()}">{self.target_text}</span>')
                 self.setStyleSheet("font-size: 50px; padding: 20px; border: 3px solid #2ecc71;")
+                print(f"O numero de erros foi {self.errors}")
+
+        else:
+            self.errors += 1
+            self.update_dysplay("#e74c3c")
 
 class MainWindow(QWidget):
     def __init__(self):
